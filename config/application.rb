260ed8eee6306b4db2de9d273abd1f89b2edad7d
Kristina Peru <krissy.germanov@gmail.com>
2022-09-26 14:29:59 -0600

    cors

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module EsgArtBack
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :post, :options]
+      end
+    end
   end
 end
+
+
